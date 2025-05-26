ac4a006b243bc5c4c7e96b2d534567d842f414ab
sanghak lee <yee950419@naver.com>
2025-05-26 15:14:42 +0900

    ; 제목 Feat: 프로젝트 세팅
    ; 본문
    
    ; 꼬릿말
    
    ; ========================
    ; 제목: "Type: Subject" 형태로 작성 / 50자 이내 / 끝에 마침표 금지
    ; 본문: 선택사항 / 커밋에 설명이 필요한 경우 / 한 줄 72자 이내
    ; 꼬릿말: 선택사항 / issue tracker ID를 명시하고 싶은 경우에 작성
    ; ========================
    ; Type
    ;   - Feat: 새로운 기능 추가
    ;   - Fix: 버그 수정
    ;   - Docs: 문서 수정
    ;   - Style: 코드 포맷 변경, 세미콜론 누락, 오타, 변수명 변경 등; 코드 수정이 없는 경우
    ;   - Refactor: 코드 리팩토링
    ;   - Test: 테스트 추가, 테스트 리팩토링; 프로덕션 코드 수정이 없는 경우
    ;   - Chore: 빌드 태스크 업데이트, 패키지 매니저 실행 등; 프로덕션 코드 수정이 없는 경우
    ;   - Design: CSS 등 사용자 UI 디자인 변경
    ;   - Comment: 필요한 주석 추가 및 변경
    ;   - Rename: 파일, 폴더명 수정 및 이동
    ;   - Remove: 파일 삭제
    ;   - Conflict: 머지 시 충돌을 해결한 경우
    ;   - Merge: 브랜치명 into develop
    ;   - !HOTFIX: 급하게 치명적인 버그를 고쳐야 하는 경우
    ; ========================
    ; Footer
    ;   - Resolves: 이슈를 해결했을 때 사용
    ;   - See also: 참고할 이슈가 있을 때 사용
    ;   - Fixes: 이슈 수정중 (아직 해결되지 않은 경우)
    ;   - Related to: 해당 커밋에 관련된 이슈번호 (아직 해결되지 않은 경우)
    ; ========================

---
+++ src/main/java/com/stardy/poker_defense/PokerDefenseApplication.java
@@ -0,0 +1,13 @@
+package com.stardy.poker_defense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerDefenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerDefenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stardy/poker_defense/PokerDefenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stardy.poker_defense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerDefenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
